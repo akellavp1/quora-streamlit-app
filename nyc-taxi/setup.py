7baec33d73f1e8fe38cc59f73261db0ac29c57bf
akellavp1 <43285035+akellavp1@users.noreply.github.com>
2024-05-04 16:58:06 +1000

    After DVC & Cokkiecutter

---
+++ nyc-taxi/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project for predicting taxi trip time in NYC',
+    author='Vara',
+    license='MIT',
+)
